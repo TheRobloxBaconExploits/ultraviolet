1df6c4c8214069499c8c799cdad278162768b5c4
TRB <72721305+TheRobloxBaconExploits@users.noreply.github.com>
2026-04-29 20:32:08 -0400

    Initialize Express server with routing and static files
    Set up an Express server with static file serving and request routing.

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { createBareServer } from '@tomphttp/bare-server-node';
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const app = express();
+const server = createServer();
+const bare = createBareServer('/bare/');
+
+app.use(express.static(join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'public', 'index.html'));
+});
+
+server.on('request', (req, res) => {
+    if (bare.shouldRoute(req)) {
+        bare.routeRequest(req, res);
+    } else {
+        app(req, res);
+    }
+});
+
+server.on('upgrade', (req, socket, head) => {
+    if (bare.shouldRoute(req)) {
+        bare.routeUpgrade(req, socket, head);
+    } else {
+        socket.end();
+    }
+});
+
+server.listen(process.env.PORT || 3000);
